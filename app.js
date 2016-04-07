aac7998e0ec516f13087c08d635dc717a645e919
Flo.W <florencewu@FloW-2.local>
2016-04-07 21:37:33 +0800

    add styl

--- app.js
+++ app.js
@@ -1,32 +1,76 @@
-/*
- * Module dependencies
- */
-var express = require('express')
-    , stylus = require('stylus')
-    , nib = require('nib')
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var nib = require('nib');
 
-//start up express
-var app = express()
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+//compile styl to css
 function compile(str, path) {
     return stylus(str)
-      .set('filename', path)
-      .use(nib())
+        .set('filename', path)
+        .set('compress', true)
+        .use(nib())
 }
 
-//link to jade files in use
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-//'middleware' functions
-app.use(express.logger('desv'))
-app.use(stylus.middleware(
-    { src: __dirname + '/public'
-      , compile: compile
-    }
-))
-app.use(express.static(__dirname + '/public'))
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.get('/', function (req, res) {
-    res.end('Hi there!')
-})
-app.listen(3000)
+app.use(require('stylus').middleware(
+    // {src: path.join(__dirname, '/public'),
+    {src: path.join(__dirname, '/public'),
+     compile: compile
+    }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //takes the name of the view to render
+  // followed by an object whose properties the view will have access to
+  res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
